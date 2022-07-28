d4bc934885a0d7904a90a57d02f0c22656146529
Aditya Bhardwaj <aditya.1998bhardwaj@gmail.com>
2022-07-28 23:56:24 +0530

    Update NLP function to return matching question from input

--- Nlp.py
+++ Nlp.py
@@ -1,41 +1,28 @@
 from datasketch import MinHash, MinHashLSH
 from nltk import ngrams
 
-questions = [
-     "Which of the following are energy fruits",
-     "Which of the following are strong fruits",
-     "Which of the following is not a fruit",
-     "Which of the following is a vegetable"
-]
+def getSimilarQuestion(questions, target):
 
-target = "What are strong fruits"
+    # Create an MinHashLSH index optimized for Jaccard threshold 0.7,
+    # that accepts MinHash objects with 128 permutations functions
+    lsh = MinHashLSH(threshold=0.7, num_perm=128)
 
-data = ['minhash is a probabilistic data structure for estimating the similarity between datasets',
-  'finhash dis fa frobabilistic fata ftructure for festimating the fimilarity fetween fatasets',
-  'weights controls the relative importance between minizing false positive',
-  'wfights cfntrols the rflative ifportance befween minizing fflse posftive',
-]
+    # Create MinHash objects
+    minhashes = {}
+    for index, value in enumerate(questions):
+        minhash = MinHash(num_perm=128)
+        for gram in ngrams(value, 3):
+            minhash.update("".join(gram).encode('utf-8'))
+        lsh.insert(index, minhash)
+        minhashes[index] = minhash
 
+    minhash = MinHash(num_perm=128)
+    for d in ngrams(target, 3):
+        minhash.update("".join(d).encode('utf-8'))
+    lsh.insert(len(questions), minhash)
+    minhashes[len(questions)] = minhash
 
-
-# Create an MinHashLSH index optimized for Jaccard threshold 0.5,
-# that accepts MinHash objects with 128 permutations functions
-lsh = MinHashLSH(threshold=0.7, num_perm=128)
-
-# Create MinHash objects
-minhashes = {}
-for index, value in enumerate(questions):
-  minhash = MinHash(num_perm=128)
-  for gram in ngrams(value, 3):
-    minhash.update("".join(gram).encode('utf-8'))
-  lsh.insert(index, minhash)
-  minhashes[index] = minhash
-
-minhash = MinHash(num_perm=128)
-for d in ngrams(target, 3):
-  minhash.update("".join(d).encode('utf-8'))
-lsh.insert(len(questions), minhash)
-minhashes[len(questions)] = minhash
-
-result = lsh.query(minhashes[len(questions)])
-print("Candidates with Jaccard similarity > 0.5 for input", target, ":", result)
+    result = lsh.query(minhashes[len(questions)])
+    print("Candidates with Jaccard similarity > 0.7 for input", target, ":", result)
+    result = [questions[i] for i in range(len(questions)) if i in result]
+    return result
