41c5bf57e3b51ef99de40196f13bb75eaa3a10b0
Aditya Bhardwaj <aditya.1998bhardwaj@gmail.com>
2022-07-28 23:56:32 +0530

    Add API endpoint

---
+++ api.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask, jsonify, request,make_response
+from dotenv import load_dotenv
+
+from nlp import getSimilarQuestion
+
+app = Flask(__name__)
+load_dotenv()
+
+@app.route('/api/similarity', methods=['POST'])
+def similarity():
+    if request.method == 'POST':
+        data = request.get_json()
+        result = getSimilarQuestion(data['questions'], data['target'])
+        return make_response(jsonify(result), 200)
+    return make_response(jsonify({'error': 'Invalid request'}), 400)
+
+
+if __name__ == '__main__':
+    app.run( host=os.environ['IP'], port=os.environ['PORT'] ,debug=True)
